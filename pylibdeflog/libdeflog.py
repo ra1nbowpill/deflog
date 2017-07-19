ae52155d1ca593b3498ef511b3933cb3cc8f0fab
ra1nbowpill <ygor.gallina@gmail.com>
2017-07-19 20:36:59 +0200

    Made libdeflog.py PEP8 compliant

--- pylibdeflog/libdeflog.py
+++ pylibdeflog/libdeflog.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
-#*********************************************************
-#* DeFlog 2008-11-02                                     *
-#* Traduce Fotolog/SMS a español                         *
-#* http://www.santiagobruno.com.ar/programas.html#deflog *
-#* Licencia: GPL v3                                      *
-#********************************************************/
+# *********************************************************
+# * DeFlog 2008-11-02                                     *
+# * Traduce Fotolog/SMS a español                         *
+# * http://www.santiagobruno.com.ar/programas.html#deflog *
+# * Licencia: GPL v3                                      *
+# ********************************************************/
 
 import re
-from html.entities import name2codepoint 
+from html.entities import name2codepoint
 
-#<code from http://www.gossamer-threads.com/lists/python/python/623437>
+# <code from http://www.gossamer-threads.com/lists/python/python/623437>
 EntityPattern = re.compile(u'&(?:#(\d+)|(?:#x([\da-fA-F]+))|([a-zA-Z]+));')
 
+
 def decodeEntities(s, encoding='utf-8'):
+    # </code from http://www.gossamer-threads.com/lists/python/python/623437>
     def unescape(match):
         code = match.group(1)
         if code:
@@ -26,9 +28,7 @@ def decodeEntities(s, encoding='utf-8'):
         if code in name2codepoint:
             return chr(name2codepoint[code])
         return match.group(0)
-
-    return EntityPattern.sub(unescape, s) 
-#</code from http://www.gossamer-threads.com/lists/python/python/623437>
+    return EntityPattern.sub(unescape, s)
 
 
 def dessimbolizar(word, format='html'):
@@ -40,20 +40,22 @@ def dessimbolizar(word, format='html'):
     word = re.sub(r"(\w+?)¡", r"\1i", word)
     return word
 
+
 def deleet(word, format='html'):
     if not re.match(r"\b\d+\b", word):
-        word = word.replace(u'0',u'o')
-        word = word.replace(u'1',u'i')
-        word = word.replace(u'3',u'e')
-        word = word.replace(u'4',u'a')
-        word = word.replace(u'5',u's')
-        word = word.replace(u'7',u't')
+        word = word.replace(u'0', u'o')
+        word = word.replace(u'1', u'i')
+        word = word.replace(u'3', u'e')
+        word = word.replace(u'4', u'a')
+        word = word.replace(u'5', u's')
+        word = word.replace(u'7', u't')
     return word
+
 
 def desalternar(word, format='html'):
     """
     Convierte palabras con mayusculas y minusculas alternadas a minusculas
-    
+
     >>> desalternar(u"hola")
     u'hola'
     >>> desalternar(u"Hola")
@@ -65,7 +67,7 @@ def desalternar(word, format='html'):
     >>> desalternar(u"hOlA")
     u'hola'
     """
-    
+
     if word.isupper() or word.islower() or word.istitle():
         return word
     else:
@@ -75,7 +77,7 @@ def desalternar(word, format='html'):
 def desmultiplicar(word, format='html'):
     """
     Elimina las repeticiones de letras
-    
+
     >>> desmultiplicar(u"hola")
     u'hola'
     >>> desmultiplicar(u"holaaaaaaa")
@@ -92,50 +94,49 @@ def desmultiplicar(word, format='html'):
     u'www'
     """
 
-    exceptions = [u'http',u'www',u'://', u'ss', u'ppio', u'ff', u'bb', u'kiss']
+    exceptions = [u'http', u'www', u'://', u'ss', u'ppio', u'ff', u'bb', u'kiss']
 
     lword = word.lower()
 
-    if re.match(r"^bs[s]+$",lword):
+    if re.match(r"^bs[s]+$", lword):
         return u'besos'
 
-    if re.match(r"^mm[m]+[h]*$",lword):
+    if re.match(r"^mm[m]+[h]*$", lword):
         return u'mmmh'
 
-    if re.match(r"^aa[a]+[h]*$",lword):
+    if re.match(r"^aa[a]+[h]*$", lword):
         return u'aaah'
 
-    if re.match(r"^ba[a]+[h]*$",lword):
+    if re.match(r"^ba[a]+[h]*$", lword):
         return u'baaah'
 
-    if re.match(r"^bu[u]+[h]*$",lword):
+    if re.match(r"^bu[u]+[h]*$", lword):
         return u'buuu'
-    
-    if not lword in exceptions and lword[:3] != u'...' :
-        if re.match(r"(^|[^l]+)[l][l]([^l])+",lword):
+
+    if lword not in exceptions and lword[:3] != u'...':
+        if re.match(r"(^|[^l]+)[l][l]([^l])+", lword):
             pos = lword.index(u'll')
-            return desmultiplicar(lword[:pos]) + u'll' + desmultiplicar(lword[pos+2:])
+            return desmultiplicar(lword[:pos]) + u'll' + desmultiplicar(lword[pos + 2:])
 
-        if re.match(r"([^r])+[r][r]([^r])+",lword):
+        if re.match(r"([^r])+[r][r]([^r])+", lword):
             pos = lword.index(u'rr')
-            return desmultiplicar(lword[:pos]) + u'rr' + desmultiplicar(lword[pos+2:])
+            return desmultiplicar(lword[:pos]) + u'rr' + desmultiplicar(lword[pos + 2:])
 
-        ##Los dos casos siguientes se pueden sacar si no se esperan muchas
-        ##palabras en inglés. Sirve para pasar google, good, teen, etc
-        if re.match(r"([^o])+[o][o]([^o])+",lword):
+        # Los dos casos siguientes se pueden sacar si no se esperan muchas
+        # palabras en inglés. Sirve para pasar google, good, teen, etc
+        if re.match(r"([^o])+[o][o]([^o])+", lword):
             pos = lword.index(u'oo')
-            return desmultiplicar(lword[:pos]) + u'oo' + desmultiplicar(lword[pos+2:])
-        
-        if re.match(r"([^e])+[e][e]([^e])+",lword):
-            pos = lword.index(u'ee')
-            return desmultiplicar(lword[:pos]) + u'ee' + desmultiplicar(lword[pos+2:])
+            return desmultiplicar(lword[:pos]) + u'oo' + desmultiplicar(lword[pos + 2:])
 
-        return re.sub(r'(.)\1+',r'\1',word)
+        if re.match(r"([^e])+[e][e]([^e])+", lword):
+            pos = lword.index(u'ee')
+            return desmultiplicar(lword[:pos]) + u'ee' + desmultiplicar(lword[pos + 2:])
+
+        return re.sub(r'(.)\1+', r'\1', word)
     else:
-        return word;
-    
-    
-    
+        return word
+
+
 def desms(word, format='html'):
     """
     Reemplaza algunas abreviaturas tipicas del sms
@@ -157,7 +158,7 @@ def desms(word, format='html'):
     >>> desms(u'ak', format='plain')
     u'ac\xe1'
     """
-    
+
     translations = {u'+': u'm&aacute;s',
                     u'+a': u'masa',
                     u'ad+': u'adem&aacute;s',
@@ -283,6 +284,7 @@ def desms(word, format='html'):
 
     return word
 
+
 def desestupidizar(word, format='html'):
     """
     Corrige algunas estupideces
@@ -300,92 +302,93 @@ def desestupidizar(word, format='html'):
     >>> desestupidizar(u'taz', format='plain')
     u'est\xe1s'
     """
-    translations = {u'10pre': u'siempre',
-                    u'arre': u'&lt;alguna sensaci&oacute;n&gt;',
-                    u'ahrre': u'&lt;alguna sensaci&oacute;n&gt;',
-                    u'ahre': u'&lt;alguna sensaci&oacute;n&gt;',
-                    u'ai': u'ah&iacute;/hay/ay',
-                    u'aios': u'adi&oacute;s',
-                    u'aka': u'ac&aacute;',
-                    u'aki': u'aqu&iacute;',
-                    u'akí': u'aqu&iacute;',
-                    u'anio': u'a&ntilde;o',
-                    u'bai': u'bye',
-                    u'ben': u'bien',
-                    u'bem': u'bien',
-                    u'bue': u'bueno',
-                    u'bzo': u'beso',
-                    u'doi': u'doy',
-                    u'efe': u'favorito',
-                    u'efeo': u'agrego a mis favoritos',
-                    u'efen': u'agreguen a favoritos',
-                    u'efes': u'favoritos',
-                    u'efs': u'favoritos',
-                    u'estoi': u'estoy',
-                    u'ff': u'favoritos',
-                    u'fs': u'favoritos',
-                    u'grax': u'gracias',
-                    u'groxo': u'grosso',
-                    u'hai': u'hay',
-                    u'hoi': u'hoy',
-                    u'i': u'y',
-                    u'ia': u'ya',
-                    u'io': u'yo',
-                    u'ise': u'hice',
-                    u'iwal': u'igual',
-                    u'kmo': u'como',
-                    u'kn': u'con',
-                    u'oi': u'hoy',
-                    u'moy': u'muy',
-                    u'muchio': u'mucho',
-                    u'mu': u'muy',
-                    u'mui': u'muy',
-                    u'nah': u'nada',
-                    u'nuche': u'no se',
-                    u'nus': u'nos',
-                    u'nuse': u'no se',
-                    u'ola': u'hola',
-                    u'olas': u'hola',
-                    u'olaz': u'hola',
-                    u'pic': u'foto',
-                    u'pick': u'foto',
-                    u'pik': u'foto',
-                    u'plis': u'por favor',
-                    u'pliz': u'por favor',
-                    u'plz': u'por favor',
-                    u'sho': u'yo',
-                    u'sip': u'si',
-                    u'soi': u'soy',
-                    u'stoi': u'estoy',
-                    u'sullo': u'suyo',
-                    u'ta': u'est&aacute;',
-                    u'tawena': u'est&aacute; buena',
-                    u'tap': u'top',
-                    u'taz': u'est&aacute;s',
-                    u'teno': u'tengo',
-                    u'toi': u'estoy',
-                    u'toos': u'todos',
-                    u'tullo': u'tuyo',
-                    u'lav': u'love',
-                    u'lov': u'love',
-                    u'lendo': u'lindo',
-                    u'llendo': u'yendo',
-                    u'mua': u'besos',
-                    u'muak': u'besos',
-                    u'muac': u'besos',
-                    u'muack': u'besos',
-                    u'muto': u'mucho',
-                    u'nah': u'nada',
-                    u'nu': u'no',
-                    u'nueo': u'nuevo',
-                    u'nunk': u'nunca',
-                    u'nuc': u'no se',
-                    u'voi': u'voy',
-                    u'we': u'bueno',
-                    u'wem': u'bueno',
-                    u'wno': u'bueno',
-                    u'xau': u'chau',
-                    u'xfa': u'por favor'
+    translations = {
+                        u'10pre': u'siempre',
+                        u'arre': u'&lt;alguna sensaci&oacute;n&gt;',
+                        u'ahrre': u'&lt;alguna sensaci&oacute;n&gt;',
+                        u'ahre': u'&lt;alguna sensaci&oacute;n&gt;',
+                        u'ai': u'ah&iacute;/hay/ay',
+                        u'aios': u'adi&oacute;s',
+                        u'aka': u'ac&aacute;',
+                        u'aki': u'aqu&iacute;',
+                        u'akí': u'aqu&iacute;',
+                        u'anio': u'a&ntilde;o',
+                        u'bai': u'bye',
+                        u'ben': u'bien',
+                        u'bem': u'bien',
+                        u'bue': u'bueno',
+                        u'bzo': u'beso',
+                        u'doi': u'doy',
+                        u'efe': u'favorito',
+                        u'efeo': u'agrego a mis favoritos',
+                        u'efen': u'agreguen a favoritos',
+                        u'efes': u'favoritos',
+                        u'efs': u'favoritos',
+                        u'estoi': u'estoy',
+                        u'ff': u'favoritos',
+                        u'fs': u'favoritos',
+                        u'grax': u'gracias',
+                        u'groxo': u'grosso',
+                        u'hai': u'hay',
+                        u'hoi': u'hoy',
+                        u'i': u'y',
+                        u'ia': u'ya',
+                        u'io': u'yo',
+                        u'ise': u'hice',
+                        u'iwal': u'igual',
+                        u'kmo': u'como',
+                        u'kn': u'con',
+                        u'oi': u'hoy',
+                        u'moy': u'muy',
+                        u'muchio': u'mucho',
+                        u'mu': u'muy',
+                        u'mui': u'muy',
+                        u'nah': u'nada',
+                        u'nuche': u'no se',
+                        u'nus': u'nos',
+                        u'nuse': u'no se',
+                        u'ola': u'hola',
+                        u'olas': u'hola',
+                        u'olaz': u'hola',
+                        u'pic': u'foto',
+                        u'pick': u'foto',
+                        u'pik': u'foto',
+                        u'plis': u'por favor',
+                        u'pliz': u'por favor',
+                        u'plz': u'por favor',
+                        u'sho': u'yo',
+                        u'sip': u'si',
+                        u'soi': u'soy',
+                        u'stoi': u'estoy',
+                        u'sullo': u'suyo',
+                        u'ta': u'est&aacute;',
+                        u'tawena': u'est&aacute; buena',
+                        u'tap': u'top',
+                        u'taz': u'est&aacute;s',
+                        u'teno': u'tengo',
+                        u'toi': u'estoy',
+                        u'toos': u'todos',
+                        u'tullo': u'tuyo',
+                        u'lav': u'love',
+                        u'lov': u'love',
+                        u'lendo': u'lindo',
+                        u'llendo': u'yendo',
+                        u'mua': u'besos',
+                        u'muak': u'besos',
+                        u'muac': u'besos',
+                        u'muack': u'besos',
+                        u'muto': u'mucho',
+                        u'nah': u'nada',
+                        u'nu': u'no',
+                        u'nueo': u'nuevo',
+                        u'nunk': u'nunca',
+                        u'nuc': u'no se',
+                        u'voi': u'voy',
+                        u'we': u'bueno',
+                        u'wem': u'bueno',
+                        u'wno': u'bueno',
+                        u'xau': u'chau',
+                        u'xfa': u'por favor'
                    }
 
     lword = word.lower()
@@ -393,20 +396,19 @@ def desestupidizar(word, format='html'):
         word = lword.replace(u'emd', u'ind')
     if u'md' in lword:
         word = lword.replace(u'md', u'nd')
-    if re.match(r'^[i]+$',lword):
+    if re.match(r'^[i]+$', lword):
         word = u'y'
     if len(lword) > 2 and lword[:3] == u'wen':
         word = u"buen" + lword[3:]
     if len(lword) > 2 and lword[:3] == u'oka':
         word = u"ok"
 
-    #posible risa
+    # posible risa
     if len(lword) > 6:
-        if re.match(u"^((j|a|k)+)$",lword):
+        if re.match(u"^((j|a|k)+)$", lword):
             return u"jajaja"
-        elif len(lword) > 8 and re.match(u"^((j|a|k|h|l|s|d|ñ)+)j((j|a|k|h|l|s|d|ñ)+)j((j|a|k|h|l|s|d|ñ)*)$",lword):
+        elif len(lword) > 8 and re.match(u"^((j|a|k|h|l|s|d|ñ)+)j((j|a|k|h|l|s|d|ñ)+)j((j|a|k|h|l|s|d|ñ)*)$", lword):
             return u"jajaja"
-
 
     if word.lower() in translations:
         word = translations[word.lower()]
@@ -416,30 +418,31 @@ def desestupidizar(word, format='html'):
 
     return word
 
+
 def desk(word, format='html'):
     exceptions = [u'kiosco', u'kilo', u'kiló', u'fuck', u'punk', u'rock', u'look', u'kiss']
-    vocales = [u'a', u'e', u'i', u'o',u'u']
+    vocales = [u'a', u'e', u'i', u'o', u'u']
 
     lword = word.lower()
 
     if lword == u'ok':
         return word
-    
+
     for a in exceptions:
         if a in lword:
             return word
-    
+
     if u"ki" in lword:
         word = word.lower().replace(u'ki', u'qui')
-    
+
     if u"ke" in word:
         word = word.lower().replace(u'ke', u'que')
 
     if len(word) > 2 and word[0].lower() == u'k' and word[1].lower() not in vocales:
         word = u"ka" + word[1:].lower()
-    
-    word = word.replace(u'k',u'c')
-    word = word.replace(u'K',u'C')
+
+    word = word.replace(u'k', u'c')
+    word = word.replace(u'K', u'C')
     return word
 
 
@@ -460,13 +463,13 @@ def deszezear(word, format='html'):
     exceptions = [u'arroz', u'feliz', u'zorr', u'azul', u'azucar', u'azúcar', u'conoz', u'zapa']
 
     lword = word.lower()
-    
+
     for a in exceptions:
         if a in lword:
             return word
-    
-    word = word.replace(u'z',u's')
-    word = word.replace(u'Z',u'S')
+
+    word = word.replace(u'z', u's')
+    word = word.replace(u'Z', u'S')
     return word
 
 
@@ -491,20 +494,20 @@ def fixmissingvowels(word, format='html'):
         u'comprarte'
     """
     exceptions = [u'get', u'cat', u'that', u'best', u'post', u'net', u'chat']
-    vocales = [u'a', u'e', u'i', u'o',u'u']
-    followsd = [u'a', u'e', u'i', u'o',u'u', u'r', u'h', u'y']
+    vocales = [u'a', u'e', u'i', u'o', u'u']
+    followsd = [u'a', u'e', u'i', u'o', u'u', u'r', u'h', u'y']
 
     lword = word.lower()
-    if not lword in exceptions:
+    if lword not in exceptions:
         if len(lword) > 1 and lword[0] == u'n' and not lword[1] in vocales:
             lword = u"en" + lword[1:]
             word = lword
-        
+
         wlen = len(word)
-        if wlen > 1 and lword[wlen-1] == u't':
+        if wlen > 1 and lword[wlen - 1] == u't':
             lword += u'e'
             word = lword
-        
+
         if len(lword) > 2 and lword[:2] == u"vr" and not lword[2] in vocales:
             lword = u"ver" + lword[2:]
             word = lword
@@ -527,6 +530,7 @@ def fixmissingvowels(word, format='html'):
 def _test():
     import doctest
     doctest.testmod()
- 
+
+
 if __name__ == "__main__":
     _test()
